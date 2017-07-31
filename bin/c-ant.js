bee659e4e2c25d608c0af9c94871692e56e7e3c7
cate.zhao <zhao.jing@wowoohr.com>
2017-07-31 16:10:04 +0800

    init

---
+++ bin/c-ant.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+var download = require('download-git-repo')
+var program = require('commander');
+var ora = require('ora')
+var exists = require('fs').existsSync
+var rm = require('rimraf').sync
+
+program
+    .version('0.0.1');
+program
+    .command('init')
+    .action(function() {
+        downloadAndGenerate(program.args[0])
+    });
+
+program.parse(process.argv);
+
+
+function downloadAndGenerate (tmp) {
+  var spinner = ora('start init the project')
+  spinner.start()
+  // Remove if local template exists
+  if (exists(tmp)) rm(tmp)
+  download("catezhao1985/ant-demo", tmp, { clone: false }, function (err) {
+    spinner.stop()
+    if (err) logger.fatal('Failed to download repo ' + template + ': ' + err.message.trim())    
+  })
+}
